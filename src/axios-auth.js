037280a63025a518b06892423ce3fb8c03bac8b4
Egawa Akiya <egawanomacbook2@EgawanoMacBook-Pro.local>
2022-09-22 20:16:25 +0900

    section17 finished

---
+++ src/axios-auth.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL:
+        'https://firestore.googleapis.com/v1/projects/vuejs-http-41e84/databases/(default)/documents'
+});
+
+export default instance;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios';
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 
+  "https://firestore.googleapis.com/v1/projects/vuejs-http-41e84/databases/(default)/documents";
+
+const interceptorsRequest = axios.interceptors.request.use(
+  config => {
+    console.log('interceptors request', config);
+    return config
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+const interceptorsResponse = axios.interceptors.response.use(
+  config => {
+    console.log('interceptors request', config);
+    return config
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.request.eject(interceptorsRequest);
+axios.interceptors.response.eject(interceptorsResponse);
 
 new Vue({
   render: h => h(App),
