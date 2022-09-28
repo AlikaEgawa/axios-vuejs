400ece2167c0346102697b381ee55c0a1c81493f
Egawa Akiya <egawanomacbook2@EgawanoMacBook-Pro.local>
2022-09-28 18:13:01 +0900

    #255 リフレッシュトークンでトークン切れしても動かす

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -30,12 +30,12 @@
     methods: {
     login() {
         this.$store.dispatch('login', {
-          email: this.email,
-          password: this.password
+			email: this.email,
+			password: this.password
         });
-        this.email = '';
-        this.password = '';
-      }
+			this.email = '';
+			this.password = '';
+		}
     },
 	}
 </script>
