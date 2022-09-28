ee2b98ecbe4842d03d3d78550796c0c6402238ad
Egawa Akiya <egawanomacbook2@EgawanoMacBook-Pro.local>
2022-09-28 16:09:26 +0900

    #252 ログイン機能実装 リダイレクトしてない

---
+++ src/store/index.js
@@ -0,0 +1,49 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from '../axios-auth';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        idToken: null
+    },
+    getters:{
+        idToken: state => state.idToken
+    },
+    mutations: {
+        updateIdToken(state, idToken) {
+            state.idToken = idToken;
+        },
+    },
+    actions: {
+        login({ commit }, authData) {
+            axios
+                .post(
+                    '/accounts:signInWithPassword?key=AIzaSyDMPhJVVKg8iyHRu5X6vJmd3BTRP9D1Ky4',
+                    {
+                        email: authData.email,
+                        password: authData.password,
+                        returnSecureToken: true
+                    }
+                )
+                .then(response => {
+                    commit('updateIdToken', response.data.idToken);
+            });
+        },
+        register({ commit }, authData) {
+            axios
+                .post(
+                    '/accounts:signUp?key=AIzaSyDMPhJVVKg8iyHRu5X6vJmd3BTRP9D1Ky4',
+                    {
+                        email: authData.email,
+                        password: authData.password,
+                        returnSecureToken: true
+                    }
+                )
+                .then(response => {
+                    commit('updateIdToken', response.data.idToken);
+            });
+        },
+    }
+});
---
+++ src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+	<div style="background-color: #9ED2C6; height: 300px; padding-top: 50px;">
+		<h2>ログイン</h2>
+		<label for="email">Email：</label>
+		<input
+			type="email"
+			id="email"
+			v-model="email"
+		>
+		<br><br>
+		<label for="password">パスワード：</label>
+		<input
+			id="password"
+      type="password"
+			v-model="password"
+		>
+		<br><br>
+		<button @click="login">送信</button>
+	</div>
+</template>
+	
+<script>
+	export default {
+		data() {
+			return {
+				email: '',
+				password: '',
+			};
+		},
+    methods: {
+      login() {
+        this.$store.dispatch('login', {
+          email: this.email,
+          password: this.password
+        });
+        this.email = '';
+        this.password = '';
+      }
+    },
+	}
+</script>
